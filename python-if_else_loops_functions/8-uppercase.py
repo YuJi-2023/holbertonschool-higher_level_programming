3d62b12c5b3da419c4d86eee4d133db4fb8c6f56
cohort20 <sophia.jiyu@gmail.com>
2023-05-31 04:50:06 -0700

    change mystr back to str

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(mystr):
+def uppercase(str):
     new_str = ""
-    for c in mystr:
+    for c in str:
         if ord(c) in range(97, 123):
             new_str = new_str + chr(ord(c) - 32)
         else:
