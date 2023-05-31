d491f7b9def33a0e266a2ce00f85501c15a1d44d
cohort20 <sophia.jiyu@gmail.com>
2023-05-31 04:30:41 -0700

    8-uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for c in str:
+        if ord(c) in range(97, 123):
+            new_str = new_str + chr(ord(c) - 32)
+        else:
+            new_str = new_str + c
+    return new_str
