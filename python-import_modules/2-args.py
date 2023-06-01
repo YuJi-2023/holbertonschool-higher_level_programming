bd18b3c1730211100dda28fd3262236d21f84709
cohort20 <sophia.jiyu@gmail.com>
2023-05-31 21:52:06 -0700

    update code to prevent execute when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import sys
 
-count = len(sys.argv) - 1
-if count == 1:
-    print(f"{count} argument:")
-    print(f"1: {sys.argv[1]}")
-elif count > 1:
-    print(f"{count} arguments:")
-    for i in range(1, count + 1):
-        print(f"{i}: {sys.argv[i]}")
-else:
-    print(f"{count} arguments.")
+def show_argv():
+    count = len(sys.argv) - 1
+    if count == 1:
+        print(f"{count} argument:")
+        print(f"1: {sys.argv[1]}")
+    elif count > 1:
+        print(f"{count} arguments:")
+        for i in range(1, count + 1):
+            print(f"{i}: {sys.argv[i]}")
+    else:
+        print("0 arguments.")
+
+if __name__ == "__main__":
+    show_argv()
