fc732b97e930b1431f1003b8a0e2b00beecb47a8
cohort20 <sophia.jiyu@gmail.com>
2023-05-31 18:10:09 -0700

    update code

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,5 @@
 def print_last_digit(number):
     number_to_str = str(number)
     last_digit = number_to_str[-1]
-    print(f"{last_digit}", end='')
+    print(f"{int(last_digit)}", end='')
     return int(last_digit)
