f098655acac6e636f02a25878e44e98be77d0691
cohort20 <sophia.jiyu@gmail.com>
2023-06-04 16:50:48 -0700

    update code

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    ele = 0
-    for ele in range(0, len(my_list)):
-        print("{0}".format(my_list[ele]))
+    index = 0
+    for index in range(0, len(my_list)):
+        print("{0}".format(my_list[index]))
