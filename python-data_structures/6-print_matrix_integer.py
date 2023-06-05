76d6aaa72d81ce66c4a1ebbfeb83c3dde7629832
cohort20 <sophia.jiyu@gmail.com>
2023-06-04 19:06:52 -0700

    add 6-print_matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for idx_of_matrix in range(len(matrix)):
+        list_in_matrix = matrix[idx_of_matrix]
+        for idx_of_list in range(len(list_in_matrix)):
+            print("{0} ".format(list_in_matrix[idx_of_list]), end='')
+        print()
