0612196b70041afc087ac3508fd2b4b8ef89fc31
cohort20 <sophia.jiyu@gmail.com>
2023-06-06 19:39:36 -0700

    update code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         sorted_keys = sorted(a_dictionary, key=a_dictionary.get)
         return sorted_keys[-1]
     else:
