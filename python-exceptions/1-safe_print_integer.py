68cbfb1f66b8d91721bf2c2191ff0926a5a247db
cohort20 <sophia.jiyu@gmail.com>
2023-06-10 05:44:35 -0700

    update 1-safe_print_int

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if type(value) is int:
-        print("{:d}".format(value))
-        return True
-    else:
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except TypeError:
         return False
