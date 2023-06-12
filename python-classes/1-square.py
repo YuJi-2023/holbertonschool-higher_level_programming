0f904290cf744737c4606e4101f5a5c245a3537f
cohort20 <sophia.jiyu@gmail.com>
2023-06-12 06:38:54 -0700

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This is a docstring for the Square class"""
+
+
+class Square:
+    """Create private instance attribute size to Square class"""
+    def __init__(self, size):
+        """Initializes with size"""
+        self.__size = size
