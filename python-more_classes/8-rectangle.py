bafed5e73c993e3ecb6a8eddf0cf5b91e3e7e1a7
cohort20 <sophia.jiyu@gmail.com>
2023-06-16 06:10:18 -0700

    docstring update

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -79,6 +79,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """return biggest rectangle"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
