b6f54bf4b6e4f05775d0add779b4dc9a9219a2ef
cohort20 <sophia.jiyu@gmail.com>
2023-06-15 18:46:55 -0700

    5-text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the "text_indentation" module.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    Args:
+        text: a string
+
+    Returns:
+        print out the formatted string to console
+    """
+    t_msg = "text must be a string"
+    m_msg = "text_indentation() missing 1 required positional argument: 'text'"
+    if not isinstance(text, str):
+        raise TypeError(t_msg)
+    for char in ['.', '?', ':']:
+        text = text.replace(char, (char + '\n\n'))
+    print(text)
