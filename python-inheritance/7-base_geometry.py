13c0b6077fbdcf3b8925fd1d543faf295fe572f6
cohort20 <sophia.jiyu@gmail.com>
2023-06-20 05:47:30 -0700

    pycodestyle for 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """validates value"""
         msg_1 = f"{name} must be an integer"
         msg_2 = f"{name} must be greater than 0"
-        if  type(value) != int:
+        if type(value) != int:
             raise TypeError(msg_1)
         if value <= 0:
             raise ValueError(msg_2)
