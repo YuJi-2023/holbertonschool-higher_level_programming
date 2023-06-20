ee5d07a3043ec4d6732413b1035da33e50b74e7f
cohort20 <sophia.jiyu@gmail.com>
2023-06-20 04:38:36 -0700

    7-base_geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """public method area()"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        msg_1 = f"{name} must be an integer"
+        msg_2 = f"{name} must be greater than 0"
+        if not isinstance(value, int):
+            raise TypeError(msg_1)
+        elif value <= 0:
+            raise ValueError(msg_2)
