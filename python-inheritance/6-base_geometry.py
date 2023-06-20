52125030c8c5251f7427b7914ab83e79eb258a40
cohort20 <sophia.jiyu@gmail.com>
2023-06-20 04:06:33 -0700

    6-base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """set to pass"""
+
+    def area(self):
+        """public method"""
+        raise Exception('area() is not implemented')
