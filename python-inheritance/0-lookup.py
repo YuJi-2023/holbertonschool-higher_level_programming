a8f8dda18571b49e0f4216ce0125a0fa2e8a93c4
cohort20 <sophia.jiyu@gmail.com>
2023-06-19 21:08:52 -0700

    documentation requirement

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""using dir() to list all attributes and methods"""
+
+
 def lookup(obj):
     """a function that returns the list of attributes and methods of an obj"""
     return dir(obj)
