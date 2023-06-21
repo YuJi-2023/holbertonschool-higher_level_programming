a7fb03d50248e127bee5e7dccf3115b6aa70b95e
cohort20 <sophia.jiyu@gmail.com>
2023-06-21 03:46:54 -0700

    10-square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class inherits Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a subclass to Rectangle"""
+
+    def __init__(self, size):
+        """initialise subclass with size"""
+        self.__size = size
+        super().integer_validator('size', size)
+
+    def area(self):
+        """implement area() method"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """str() return a formated string representation of the rectangle"""
+        return f"[Rectangle] {self.__size}/{self.__size}"
