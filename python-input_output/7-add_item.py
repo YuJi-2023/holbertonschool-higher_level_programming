311743b70f50711ae066b05353a8db2c70c7a898
cohort20 <sophia.jiyu@gmail.com>
2023-06-22 04:22:02 -0700

    7-add_item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""add to list and save to JSON file"""
+import os
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+"""add obj to JSON file and load from it after"""
+filename = "add_item.json"
+if not os.path.exists(filename):
+    my_list = []
+else:
+    my_list = load_from_json_file(filename)
+
+new_item = sys.argv[1:]
+for elem in new_item:
+    my_list.append(elem)
+save_to_json_file(my_list, filename)
