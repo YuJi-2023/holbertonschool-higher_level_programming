89e7738e51f1603703849c4a5cb4be045f2a0d25
cohort20 <sophia.jiyu@gmail.com>
2023-06-26 03:38:36 -0700

    update code

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialise data"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
