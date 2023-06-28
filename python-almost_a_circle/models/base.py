25df43f0f572c1faab12b9d68b00e70617b91be4
cohort20 <sophia.jiyu@gmail.com>
2023-06-27 23:22:10 -0700

    update base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,24 +18,23 @@ class Base:
     def to_json_string(list_dictionaries):
         """return JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return '[]'
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """write json string of list_objs to a file"""
         file_name = cls.__name__ + '.json'
+        if list_objs is None:
+            list_objs = []
+        json_str = cls.to_json_string(obj.to_dictionary() for obj in list_objs)
         with open(file_name, 'w') as myFile:
-            if list_objs is None:
-                myFile.write('[]')
-            else:
-                dict_list = [obj.to_dictionary() for obj in list_objs]
-                myFile.write(Base.to_json_string(dict_list))
+            myFile.write(json_str)
 
     @staticmethod
     def from_json_string(json_string):
         """return the list of json string representation"""
         if json_string is None:
-            return '[]'
+            return []
         else:
             return json.loads(json_string)
