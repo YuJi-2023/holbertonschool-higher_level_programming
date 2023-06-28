e46282e71fff5fd50cdee3a6db742034591cbcd3
cohort20 <sophia.jiyu@gmail.com>
2023-06-27 21:46:59 -0700

    add to_json_string for Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """define class Base"""
+import json
 
 
 class Base:
@@ -12,3 +13,11 @@ class Base:
         else:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
