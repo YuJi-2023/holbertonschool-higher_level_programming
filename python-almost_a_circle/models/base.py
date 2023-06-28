d2e114a1ac01c432c46d51c2ef7af6eb6c09175b
cohort20 <sophia.jiyu@gmail.com>
2023-06-28 05:14:39 -0700

    update code

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """return JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return '[]'
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
