50ce7620df0a5799ded00ccccef9459273c10cb7
cohort20 <sophia.jiyu@gmail.com>
2023-06-27 22:00:54 -0700

    update to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """return JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return '[]'
         else:
             return json.dumps(list_dictionaries)
