60a474822a9740c579b92c8c03b8176d054753ed
YuJi-2023 <sophia.jiyu@gmail.com>
2023-07-25 04:42:08 -0700

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates user_0d_1 with all privileges with pw
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
