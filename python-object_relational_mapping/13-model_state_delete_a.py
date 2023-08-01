7ca9eb1fb612efd36307a685256e1049e24389ba
YuJi-2023 <sophia.jiyu@gmail.com>
2023-07-31 21:21:45 -0700

    task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""deletes the State object contains a"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # connecting
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                                                                sys.argv[1],
+                                                                sys.argv[2],
+                                                                sys.argv[3]),
+                           echo=False)
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # perform delete object
+    result = session.query(State).filter(State.name.like('%a%'))
+    for state in result:
+        session.delete(state)
+
+    # close the session
+    session.commit()
